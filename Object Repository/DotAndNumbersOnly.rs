afc5d2587c94e7d506e02d18773850b33eb788c4
kazurayam <kazuaki.matsuhashi@quick.jp>
2020-07-17 17:02:04 +0900

    update

---
+++ Object Repository/DotAndNumbersOnly.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>DotAndNumbersOnly</name>
+   <tag></tag>
+   <elementGuidId>965590df-9a69-4019-876e-c36097a51f99</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>/*[string-length(normalize-space(translate(text(), '.0123456789', '')))=0]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/*[string-length(normalize-space(translate(text(), '.0123456789', '')))=0]</value>
+   </webElementProperties>
+</WebElementEntity>
